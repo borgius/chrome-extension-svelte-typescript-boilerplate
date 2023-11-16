8c9a9e441503ff967ef63487b23ca5af3ea804bf
Burdyey, Viktor <Viktor.Burdyey@fmr.com>
2023-11-16 18:34:21 -0500

    fix types for uno.config

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,16 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
+import { defineConfig, Preset, PresetOrFactory, Rule } from 'unocss'
 import { presetUno } from 'unocss'
 import presetIcons from '@unocss/preset-icons'
 
-export default defineConfig({
-  presets: [presetUno(), presetIcons({})],
+type Theme = {}
+
+export default defineConfig<Theme>({
+  presets: [
+    presetUno(),
+    presetIcons({
+      cdn: 'https://esm.sh/'
+    }) as PresetOrFactory<Theme>,
+  ],
   // ...UnoCSS options
 })
